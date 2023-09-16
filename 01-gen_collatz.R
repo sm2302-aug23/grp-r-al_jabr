43654d0783d134360f7b19c7373f510b6344f8fa
Hafizuddin Norajame <20b2062@ubd.edu.bn>
2023-09-16 10:29:16 +0800

    Update 01-gen_collatz.R (Creating function is done)

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -4,4 +4,15 @@ gen_collatz <- function(n) {
   if (n <= 0) {
     stop("Input must be a positive integer")
   }
+  seq <- c(n)
+  while (n != 1) {
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+    seq <- c(seq, n)
+  }
+  return(seq)
 }
+
