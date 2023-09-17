62c48b0fc4a2f8ff3d49aee862a95e62eb0e1930
Hafizuddin Norajame <20b2062@ubd.edu.bn>
2023-09-17 13:58:46 +0800

    Update 01-gen-collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -1,4 +1,7 @@
+library(tidyverse)
 library(tibble)
+
+# Create gen_collatz function --------------------------------------------------
 
 gen_collatz <- function(n) {
   if (n <= 0) {
@@ -15,4 +18,36 @@ gen_collatz <- function(n) {
   }
   return(seq)
 }
+
+# Create collatz_df tibble -----------------------------------------------------
+
+start <- 1:10000
+
+seq <- list()
+for (i in start) {
+  collatz_seq <- gen_collatz(i)
+  seq[[i]] <- collatz_seq
+}
+
+length <- double()
+for (i in start) {
+  length[i] <- length(gen_collatz(i))
+}
+
+parity <- ifelse(start %% 2 == 0,
+                 "Even",
+                 "Odd")
+
+max_val <- double()
+for (i in start) {
+  max_val[i] <- max(gen_collatz(i))
+}
+
+
+collatz_df <- tibble(start,
+                     seq,
+                     length,
+                     parity,
+                     max_val)
+collatz_df
 
