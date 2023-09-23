b213fc4141cb75d853f34e7900c5b57763b215d7
dnshzm <20b2044@ubd.edu.bn>
2023-09-23 17:43:01 +0800

    Update 04-plots.R

--- 04-plots.R
+++ 04-plots.R
@@ -4,31 +4,33 @@ library(ggplot2)
 # Task 4 -----------------------------------------------------------------------
 
 # Scatterplot of all the sequence lengths --------------------------------------
-# Horizontal Axis : starting integer
-# Vertical Axis   : length of the sequence
-# Identify the top 10 starting integers on your plot
-
-#Scatterplot
-ggplot( data = collatz_df,
-        mapping = aes(x = start,
-                      y = length)
+plot1<- ggplot( data = collatz_df,
+                mapping = aes(x = start,
+                              y = length)
 )+
-  geom_point()
+  geom_point()+
   labs(
     title = "Scatterplot of Sequence Lengths",
-    x = "Starting integer",
-    y = "Length of sequence"
+    x = "Starting Integer",
+    y = "Length of Sequence"
   )
 
-# Scatterplot of the highest values of starting integers -----------------------
-# Horizontal Axis : starting integer
-# Vertical Axis   : max value
-# Identify the top 10 starting integers
+# To find the top 10 longest starting integers
+sortedlength <- collatz_df %>% arrange(desc(length))
+top_10_length <- sortedlength %>%top_n(10,length)
 
-#Scatterplot
-ggplot( data = collatz_df,
-        mapping =aes(x = start,
-                     y = max_val)
+# To identify the top 10 longest starting integers in the scatterplot
+scatterplot1 <-
+  plot1 + 
+  geom_point(data = top_10_length,aes(colour = "Top 10"))+
+  scale_colour_manual(values = c("Top 10" = "blue"))
+
+scatterplot1
+
+# Scatterplot of the highest values of starting integers -----------------------
+plot2<- ggplot( data = collatz_df,
+                mapping = aes(x = start,
+                              y = max_val)
 )+
   geom_point()+
   labs(
@@ -36,7 +38,20 @@ ggplot( data = collatz_df,
     x = "Starting Integer",
     y = "Maximum Value"
   )
-  
+
+# To find the top 10 starting integers with the highest value
+sortedvalue <- collatz_df %>% arrange(desc(max_val))
+top_10_value <- sortedvalue %>% top_n(10,max_val)
+
+# To highlight the top 10 starting integers 
+scatterplot2 <-
+  plot2 + 
+  geom_point(data = top_10_value,
+             aes(colour = "Top 10"))+
+  scale_colour_manual(values = c("Top 10" = "red"))
+
+scatterplot2
+
 # Boxplot of distribution of sequence length for even,odd  starting integers----
 
 ggplot( data = collatz_df,
